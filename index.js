ed32eed474c82a8ac976efb02a1845e5ee486387
bilalcoder-28 <bilalasif2007@gamil.com>
2025-11-27 21:11:12 +0500

    hellooo js

---
+++ index.js
@@ -0,0 +1,7 @@
+let emailInput = document.getElementById("main-email")
+let passwordInput = document.getElementById("main-password")
+
+function dataHandle () {
+    console.log("User Email is ---> ",emailInput.value )
+    console.log("User Pssseord is ---> " , passwordInput.value)
+}
