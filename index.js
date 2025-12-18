e77b211e6dff7937b8cb95b89450e9eb238b3f9f
bilalcoder-28 <bilalasif2007@gamil.com>
2025-12-19 03:19:33 +0500

    updated

--- index.js
+++ index.js
@@ -1,7 +1,106 @@
 let emailInput = document.getElementById("main-email")
 let passwordInput = document.getElementById("main-password")
 
-function dataHandle () {
-    console.log("User Email is ---> ",emailInput.value )
-    console.log("User Pssseord is ---> " , passwordInput.value)
+function dataHandle() {
+    console.log("User Email is ---> ", emailInput.value)
+    console.log("User Pssseord is ---> ", passwordInput.value)
 }
+
+
+function spaceFunction() {
+
+    if (emailInput.value.trim() == "") {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Something went wrong!",
+            footer: '<a href="#">Why do I have this issue?</a>'
+        });
+    }
+
+    else if (passwordInput.value.trim() == "") {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Something went wrong!",
+            footer: '<a href="#">Why do I have this issue?</a>'
+        });
+
+    }
+
+    // else if (emailInput.value == "@") {
+    //     window.location.href = "dash.html"
+
+    // }
+
+
+
+
+    for (let i = 0; i < emailInput.value.length; i++) {
+        if (emailInput.value[i] == "@") {
+
+            Swal.fire({
+                title: "Done !!",
+                icon: "success",
+                draggable: true
+
+
+            });
+
+            if (emailInput.value[i] == ".") {
+                Swal.fire({
+                    title: "Done !!",
+                    icon: "success",
+                    draggable: true
+
+
+                });
+            }
+
+
+
+
+
+
+            window.location.href = "http://127.0.0.1:5500/dash.html"
+
+        }
+
+
+
+
+
+        else {
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Something went wrong!",
+                footer: '<a href="#">Why do I have this issue?</a>'
+            });
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+//   window.location.href = "" ; 
+
+
+
+
